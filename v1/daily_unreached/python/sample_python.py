9f3f95b024124144380112267110b2c79eb9e977
Codemis <johnathan@missionaldigerati.com>
2013-09-04 17:57:27 -0700

    Added GPL license information.

--- v1/daily_unreached/python/sample_python.py
+++ v1/daily_unreached/python/sample_python.py
@@ -1,4 +1,23 @@
 #!/usr/bin/python
+# This file is part of Joshua Project API.
+# 
+# Joshua Project API is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# Joshua Project API is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see 
+# <http://www.gnu.org/licenses/>.
+#
+# @author Johnathan Pulos <johnathan@missionaldigerati.org>
+# @copyright Copyright 2013 Missional Digerati
+# 
 # import the necessary libraries
 import json
 import urllib2
@@ -6,7 +25,7 @@ import string
 import sys
 # set some important variables
 domain = "http://jpapi.codingstudio.org"
-api_key = "4a2bab35f1c7"
+api_key = YOUR_API_KEY
 # request the API for the Daily Unreached People Group
 try:
     response = urllib2.urlopen(domain+"/v1/people_groups/daily_unreached.json?api_key="+api_key)
